1d852f78724f779dd9c1f262ea4208077605c5c3
Diarmuid Murphy <diarmuid.com@gmail.com>
2024-08-27 22:09:00 -0700

    db logic working

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,44 @@
 import express from 'express';
 import logger from 'morgan';
+import db from './config/connection.js';
+
+// pool.connect();
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
 app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('../client/dist'));
 }
 
-app.get('/health', (req, res) => {
-  res.send('Hello, World?');
+app.post('/api/users', (req, res) => {
+  console.log(req.body);
+
+  res.end();
 });
 
-app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
+app.get('/api/health', (req, res) => {
+  db.query('SELECT * FROM users', (err, result) => {
+    if (err) {
+      res.status(500).json({ err });
+      return;
+    }
+    
+    res.json(result.rows);
+  });
+});
+
+(async () => {
+  try {
+    await db.connect();
+    console.log('Connected to db...');
+    app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));  
+  } catch (error) {
+    console.error(error);
+  }
+})();
